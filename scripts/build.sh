674fa850e8a4f6fd00d8d42cbd0db2a1a0810886
xfy520 <78788091+xfy520@users.noreply.github.com>
2021-12-03 14:27:42 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,6 @@ set -e
 set -x
 
 # linux
-GOOS=linux GOARCH=amd64 go build -o release/amd64/drone-vault
-GOOS=linux GOARCH=arm64 go build -o release/arm64/drone-vault
-GOOS=linux GOARCH=arm   go build -o release/arm/drone-vault
+GOPROXY=https://goproxy.cn GOOS=linux GOARCH=amd64 go build -o release/amd64/drone-vault
+GOPROXY=https://goproxy.cn GOOS=linux GOARCH=arm64 go build -o release/arm64/drone-vault
+GOPROXY=https://goproxy.cn GOOS=linux GOARCH=arm   go build -o release/arm/drone-vault
